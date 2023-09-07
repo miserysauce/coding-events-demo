0bc1b289c7603c64cb918c84114195132a6be323
AJT <lunegeoid@gmail.com>
2023-09-07 12:58:18 -0500

    updated

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,9 +1,7 @@
 package org.launchcode.codingevents.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -20,21 +18,19 @@ public class Event extends AbstractEntity {
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+    @OneToOne(cascade=CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
 
-    @Size(max = 500, message = "Description too long!")
-    private String description;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email. Try again.")
-    private String contactEmail;
     @ManyToOne
-    @NotNull(message="Category is required")
+    @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
+
         this.eventCategory = eventCategory;
     }
 
@@ -45,26 +41,6 @@ public class Event extends AbstractEntity {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
     public EventCategory getEventCategory() {
         return eventCategory;
     }
@@ -73,9 +49,18 @@ public class Event extends AbstractEntity {
         this.eventCategory = eventCategory;
     }
 
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
     @Override
     public String toString() {
         return name;
     }
+
 
 }
